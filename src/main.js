a206b575382e4d4d8e4b50648e1bf225b41c5ec5
2823397108 <2823397108@qq.com>
2023-11-08 18:02:36 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import './plugins/axios'
+import App from './App.vue'           //vue
+import router from './router'         //路由
+import './plugins/element.js'         //element
+import * as echarts from 'echarts'    //图表
 
+Vue.prototype.$echarts = echarts;
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
